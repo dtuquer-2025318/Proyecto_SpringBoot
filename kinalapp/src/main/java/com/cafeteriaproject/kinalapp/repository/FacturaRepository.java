8ec005f14f792b7f883b8e21d2a1f992a487049f
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-02-18 09:21:33 -0600

    Delete kinalapp/src/main/java/com/cafeteriaproject/kinalapp/repository/FacturaRepository.java

--- kinalapp/src/main/java/com/cafeteriaproject/kinalapp/repository/FacturaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cafeteriaproject.kinalapp.repository;
-
-import com.cafeteriaproject.kinalapp.entity.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacturaRepository extends JpaRepository<Factura,Long> {
-}
