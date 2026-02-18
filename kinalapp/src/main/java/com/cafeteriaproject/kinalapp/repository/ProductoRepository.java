69b9eb25869bf862fefa81bcb8d46d44505efd44
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-02-18 09:21:25 -0600

    Delete kinalapp/src/main/java/com/cafeteriaproject/kinalapp/repository/ProductoRepository.java

--- kinalapp/src/main/java/com/cafeteriaproject/kinalapp/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cafeteriaproject.kinalapp.repository;
-
-import com.cafeteriaproject.kinalapp.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
